ee7bd80c0949d23d5aed821f3796b14bac351406
Gyula Vigi Menyhart <gyula02997@gmail.com>
2023-02-16 12:08:45 +0100

    Added header page

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-signin-client_id" content="987894131177-qgnc2jcciacpuoir9gckqf4i6t9ti9rs.apps.googleusercontent.com">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/style_responsive.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script src="https://kit.fontawesome.com/4a0bc6383c.js" crossorigin="anonymous" async defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <title>Jules Watches</title>
+</head>
