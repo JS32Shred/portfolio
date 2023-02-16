9d7ed0b926b111b4fc222336a6f915f632d648d1
Gyula Vigi Menyhart <gyula02997@gmail.com>
2023-02-16 12:08:39 +0100

    Added footer page

---
+++ includes/footer.php
@@ -0,0 +1,85 @@
+    <!-- Newsletter Section -->
+
+    <div class="newsLetter">
+        <div>
+            <h1>Be the first to know</h1>
+            <p>Discover how to find the best watch and enjoy exclusive products and offers via email</p>
+            <input type="text" placeholder="Your email here">
+            <div>
+                <input type="checkbox" id="accept">
+                <label for="accept">I accept the <span class="privacyPolicy">privacy policy <i class="fa fa-external-link"></i></span></label>
+            </div>
+            <button>sign up</button>
+        </div>
+    </div>
+
+    <!-- Footer Section -->
+
+    <div class="socialMedia">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-pinterest"></i></a>
+        <a href="#"><i class="fa fa-youtube-play"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+    </div>
+
+    <footer>
+        <div class="footerLinks">
+            <div>
+                <span>about <strong>jules watches</strong></span>
+                <a href="#">about us</a>
+                <a href="#">TV adverts</a>
+                <a href="#">visit our showroom</a>
+                <a href="#">FAQs</a>
+                <a href="#">discount</a>
+                <a href="#">refer a friend</a>
+            </div>
+            <div>
+                <span>contact us</span>
+                <a href="#">by telephone</a>
+                <a href="#">by email</a>
+                <a href="#">login | register</a>
+                <a href="#">press</a>
+                <a href="#">report a bug</a>
+            </div>            
+            <div>
+                <span>our policies</span>
+                <a href="#">delivery policy</a>
+                <a href="#">terms & conditions</a>
+                <a href="#">returns policy</a>
+                <a href="#">privacy & cookies</a>
+                <a href="#">shipping locations</a>
+                <a href="#">vouchers / discounts</a>
+                <a href="#">bulk purchases</a>
+                <a href="#">cookie settings</a>
+            </div>
+            <div>
+                <span>other information</span>
+                <a href="#">bracelet adjustment</a>
+                <a href="#">the edit</a>
+                <a href="#">watch reviews</a>
+                <a href="#">finance options</a>
+                <a href="#">modern slavery act transparency</a>
+                <a href="#">the watch hut</a>
+                <a href="#">lux ecom</a>
+            </div>
+        </div>
+        
+        <div class="paymentMethods">
+            <i class="fa fa-cc-amex"></i>
+            <i class="fa fa-cc-diners-club"></i>
+            <i class="fa fa-cc-discover"></i>
+            <i class="fa fa-cc-jcb"></i>
+            <i class="fa fa-cc-mastercard"></i>
+            <i class="fa fa-cc-paypal"></i>
+            <i class="fa fa-cc-stripe"></i>
+            <i class="fa fa-cc-visa"></i>
+        </div>
+        <div>
+            <strong>Jules Watches</strong> is a trading name of The Jules Watches Holdings Limited.</br>
+            Jules Watches&trade; is a registered trademark &copy; <?php echo date("Y");?>.
+        </div>
+    </footer>
+    <script src="scripts/main.js"></script>
+</body>
+</html>
